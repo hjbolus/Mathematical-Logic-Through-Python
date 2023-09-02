4ddf8d03edec59ff1fd72c9255952098d63c651f
hjbolus <141349319+hjbolus@users.noreply.github.com>
2023-09-01 22:30:16 -0500

    Create soundness.py

---
+++ propositions/soundness.py
@@ -0,0 +1,63 @@
+# This file is part of the materials accompanying the book
+# "Mathematical Logic through Python" by Gonczarowski and Nisan,
+# Cambridge University Press. Book site: www.LogicThruPython.org
+# (c) Yannai A. Gonczarowski and Noam Nisan, 2017-2022
+# File name: propositions/soundness.py
+
+"""Programmatic proof of the soundness of Propositional Logic."""
+
+from typing import Tuple
+
+from syntax import *
+from semantics import *
+from proofs import *
+
+def rule_nonsoundness_from_specialization_nonsoundness(
+        general: InferenceRule, specialization: InferenceRule, model: Model) \
+        -> Model:
+    """Demonstrates the non-soundness of the given general inference rule given
+    an example of the non-soundness of the given specialization of this rule.
+
+    Parameters:
+        general: inference rule to the soundness of which to find a
+            counterexample.
+        specialization: non-sound specialization of `general`.
+        model: model in which `specialization` does not hold.
+
+    Returns:
+        A model in which `general` does not hold.
+    """
+    assert specialization.is_specialization_of(general)
+    assert not evaluate_inference(specialization, model)
+    
+    map1 = general.specialization_map(specialization)
+    g_model = dict()
+    for key in map1.keys():
+        g_model[key] = evaluate(map1[key], model)
+    return g_model
+    # Task 4.9
+
+def nonsound_rule_of_nonsound_proof(proof: Proof, model: Model) -> \
+        Tuple[InferenceRule, Model]:
+    """Finds a non-sound inference rule used by the given valid proof of a
+    non-sound inference rule, and demonstrates the non-soundness of the former
+    rule.
+
+    Parameters:
+        proof: valid proof of a non-sound inference rule.
+        model: model in which the inference rule proved by the given proof does
+            not hold.
+
+    Returns:
+        A pair of a non-sound inference rule used in the given proof and a model
+        in which this rule does not hold.
+    """
+    assert proof.is_valid()
+    assert not evaluate_inference(proof.statement, model)
+    for i in range(len(proof.lines)):
+        if not proof.lines[i].is_assumption() and not evaluate(proof.lines[i].formula, model):
+            suspect, number = proof.lines[i], i
+            break
+    assert not is_sound_inference(suspect.rule)
+    return (suspect.rule, rule_nonsoundness_from_specialization_nonsoundness(suspect.rule, proof.rule_for_line(i), model))
+    # Task 4.10
