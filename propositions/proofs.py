692c3741a7d2126da22c5f6d43e67e750ba0a635
hjbolus <141349319+hjbolus@users.noreply.github.com>
2025-09-29 06:06:53 -0500

    Update proofs.py

--- propositions/proofs.py
+++ propositions/proofs.py
@@ -7,10 +7,8 @@
 """Proofs by deduction in Propositional Logic."""
 from __future__ import annotations
 from typing import AbstractSet, FrozenSet, List, Mapping, Optional, Sequence, Set, Tuple, Union
-import sys
-sys.path.append('/Users/harrisbolus/Desktop/Fun/Mathematical logic thru python')
-from propositions.logic_utils import frozen, memoized_parameterless_method
-from propositions.syntax import *
+from ..logic_utils import frozen, memoized_parameterless_method
+from .syntax import *
 
 #: A mapping from variable names to formulas.
 SpecializationMap = Mapping[str, Formula]
